2985e7fbec212a6e7e9be7ffc8ea1c7958ffca2e
VenuBabuBorra <venubabuborra@gmail.com>
2016-01-02 21:33:17 +0530

    Solution commit for first time

---
+++ idoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(idoc.Startup))]
+namespace idoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
