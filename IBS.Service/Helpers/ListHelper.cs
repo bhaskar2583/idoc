27ba8e16f317accbb6549181cf08b0d51a0fe87c
das <mariyadasu.nukathoti@cdk.com>
2019-09-11 22:32:32 +0530

    c

---
+++ IBS.Service/Helpers/ListHelper.cs
@@ -0,0 +1,47 @@
+﻿using IBS.Service.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBS.Service.Helpers
+{
+    public static class ListHelper
+    {
+        public static List<SelectListCommon> GetDateTypes()
+        {
+            return new List<SelectListCommon>()
+            {
+                new SelectListCommon()
+                {
+                    Id=1,
+                    Name="Payment"
+                },
+                new SelectListCommon()
+                {
+                    Id=1,
+                    Name="Created"
+                }
+
+            };
+        }
+        public static List<SelectListCommon> RType()
+        {
+            return new List<SelectListCommon>()
+            {
+                new SelectListCommon()
+                {
+                    Id=1,
+                    Name="Open"
+                },
+                new SelectListCommon()
+                {
+                    Id=1,
+                    Name="Close"
+                }
+
+            };
+        }
+    }
+}
---
+++ IBS/Controllers/ReportsController.cs
@@ -0,0 +1,32 @@
+﻿using IBS.Service.Helpers;
+using IBS.Service.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IBS.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly List<SelectListCommon> DateTypes;
+        private readonly List<SelectListCommon> RType;
+
+        public ReportsController()
+        {
+            DateTypes = ListHelper.GetDateTypes();
+            RType = ListHelper.RType();
+        }
+        // GET: Reports
+        public ActionResult Index()
+        {
+            ViewBag.DateType = DateTypes;
+            ViewBag.Companies = new List<SelectListCommon>();
+            ViewBag.Agents = new List<SelectListCommon>();
+            ViewBag.RType = RType;
+
+            return View();
+        }
+    }
+}
